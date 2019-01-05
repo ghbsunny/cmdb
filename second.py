77f5d8c234506a4977312f702a0b48cd2d1e5d40
sunny <951408320@qq.com>
2019-01-05 08:07:40 +0800

    just print helo

--- second.py
+++ second.py
@@ -1,10 +1,7 @@
 from dispatcher import Dispatcher
 
 if __name__ == '__main__':
-<<<<<<< Updated upstream
     print("sunny")
-=======
     print("welcome to www.ghbsunny.cn")
     dis = Dispatcher()
     dis.run()
->>>>>>> Stashed changes
