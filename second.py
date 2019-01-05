d355d08eedadaecf7a172fa4f82d20e86dca866d
sunny <951408320@qq.com>
2019-01-05 08:06:16 +0800

    del .idea

--- second.py
+++ second.py
@@ -1,2 +1,10 @@
+from dispatcher import Dispatcher
+
 if __name__ == '__main__':
+<<<<<<< Updated upstream
     print("sunny")
+=======
+    print("welcome to www.ghbsunny.cn")
+    dis = Dispatcher()
+    dis.run()
+>>>>>>> Stashed changes
